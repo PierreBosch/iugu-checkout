2dde986fd09cc99077690067d3e5cfda3d58a287
Pierre Boschetto <pierre.moota@gmail.com>
2022-04-12 18:19:13 -0300

    feat: config routes and create success payment page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import CustomThemeProvider from './contexts/ThemeContext';
 import { Header } from './components/Header';
-import GlobalStyle from './styles/global';
-import Checkout from './pages/Checkout';
 import OffersProvider from './contexts/OffersContext';
+import CustomThemeProvider from './contexts/ThemeContext';
+import AppRoutes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <CustomThemeProvider>
       <GlobalStyle />
+
       <div className="container">
         <Header />
         <OffersProvider>
-          <Checkout />
+          <AppRoutes />
         </OffersProvider>
       </div>
     </CustomThemeProvider>
---
+++ src/pages/PaymentSuccess/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const PaymentSuccess: React.FC = () => {
+  return <h1>ola</h1>;
+};
+
+export default PaymentSuccess;
